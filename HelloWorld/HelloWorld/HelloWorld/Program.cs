5944d31f9eda8cbf39493159b32954f5f7a28a29
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-02-06 11:55:20 +0000

    Added RoadVehicle class

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,20 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            RoadVehicle v1 = new RoadVehicle(123456);
+            RoadVehicle v2 = new RoadVehicle(246810);
+
+            if (!(v1 is null) && !(v2 is null))
+            {
+                Console.WriteLine("Vehicle 1 serial: " + v1.GetEngineSerialNumber() + " Vehicle 2 serial: " + v2.GetEngineSerialNumber());
+            }
+            else 
+            {
+                System.Console.WriteLine("Error: object is null.");
+                return;
+            }
+
             Console.ReadLine();
         }
     }
---
+++ HelloWorld/HelloWorld/HelloWorld/RoadVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class RoadVehicle
+    {
+        public static string projectVersion = "1.0";
+        private int engineSerialNumber;
+
+        public RoadVehicle() 
+        {  
+            Console.WriteLine("RoadVehicle constructor.");
+        }
+
+        public RoadVehicle(int engineSerialNumber)
+        {
+            this.engineSerialNumber = engineSerialNumber;
+        }
+
+        public int GetEngineSerialNumber()
+        {
+            return this.engineSerialNumber;
+        }
+    }
+}
